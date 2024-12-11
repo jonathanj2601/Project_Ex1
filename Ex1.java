9e6589a6ac613013bdd06bef2abd08125a8eb83a
jonathanj2601 <jonathan.juwiler@msmail.ariel.ac.il>
2024-12-11 23:22:41 +0200

    hi

--- Ex1.java
+++ Ex1.java
@@ -11,18 +11,46 @@
  * You should implement the following static functions:
  */
 public class Ex1 {
+    //Converter converts from a letter to a number. and if it is a number return a.
+    //It is known in the function that you only put A-G or 0-9
+        public static int Converter(char a){
 
+            if(a=='A')
+                return 10;
+            if(a=='B')
+                return 11;
+            if(a=='C')
+                return 12;
+            if(a=='D')
+                return 13;
+            if(a=='E')
+                return 14;
+            if(a=='F')
+                return 15;
+            if(a=='G')
+                return 16;
+            return Integer.parseInt(String.valueOf(a));
+        }
         /**
          * Convert the given number (num) to a decimal representation (as int).
-         * It the given number is not in a valid format returns -1.
+         * If the given number is not in a valid format returns -1.
          * @param num a String representing a number in basis [2,16]
          * @return
          */
         public static int number2Int(String num) {
             int ans = -1;
-            // add your code here
-
-            ////////////////////
+            if(!(isNumber(num)))//if the num isn't in the format. send back -1
+                return ans;
+            if(num.indexOf('b')==-1)//if the num doesn't have b it means the string is only made with digits. so it's already represented as decimal
+                return Integer.parseInt(num);
+            int basis= Converter(num.charAt(num.indexOf('b')+1));//converts the basis into a number
+            int order = 0;
+            ans = 0; //reset ans to see the answer
+            for(int i=num.indexOf('b')-1; i>=0;i--)//loops runs from the index right of b until the MSB
+            {
+                ans+=Converter(num.charAt(i))*(Math.pow(basis,order)); //converts each index into number and multiplies it with the basis power to the order of the number. Therefor ans becomes num represented as decimal.
+                order++;
+            }
             return ans;
         }
         /**
@@ -32,9 +60,32 @@ public class Ex1 {
          */
         public static boolean isNumber(String a) {
             boolean ans = true;
-            // add your code here
+            char c;
+            if (a.isEmpty()|| a==null) //checks if the string is null or empty
+                ans = false;
 
-            ////////////////////
+            if(a.indexOf('b')==-1 && !a.matches("\\d+")) //if 'b' isn't in the string (a). check if all the characters are digits.
+                ans = false;
+            if(a.indexOf('b')==-1 && a.matches("\\d+")) //if 'b' isn't in the string (a).and all the characters ARE digits. return true.
+                return true;
+            if(a.indexOf('b')== a.length()-1 || a.indexOf('b') == 0) //checks if there isn't a number before b or after b
+                return false;
+            if(a.charAt(a.indexOf('b')+1)=='0' || a.charAt(a.indexOf('b')+1)=='1')//checks if the base is 0 or 1
+                ans = false;
+            for(int i=0; i<a.length()-1; i++)//checks if every character is either A-G or b or 0-9
+            {
+                c = a.charAt(i);
+                if(!(Character.isDigit(c)) && !(c >= 'A' && c <= 'G') && (c!='b'))// checks if every digit is either 0-9 or A-B
+                    return false;
+            }
+            for (int j = 0; j < a.length()-1; j++) {
+                c = a.charAt(j);
+                if(c!='b')
+                {
+                    if (Converter(c)>=Converter(a.charAt(a.indexOf('b')+1)))//checks if what is before b greater than the base.
+                        ans = false;
+                }
+            }
             return ans;
         }
 
@@ -48,9 +99,45 @@ public class Ex1 {
          */
         public static String int2Number(int num, int base) {
             String ans = "";
-            // add your code here
+            if(num<0 || base<2 || base>16)
+                return ans;
+            String Sbase;
+            while(num>0){
+               if(num%base>9){
+                   if(num%base==10)
+                       ans+= 'A';
+                   if(num%base==11)
+                       ans+= 'B';
+                   if(num%base==12)
+                       ans+= 'C';
+                   if(num%base==13)
+                       ans+= 'D';
+                   if(num%base==14)
+                       ans+= 'E';
+                   if(num%base==15)
+                       ans+= 'F';
+               }
+               else {
+                   ans+= (char)num%base;
+               }
+                num/=base;
+            }
 
-            ////////////////////
+            if(base==10)
+                Sbase = "A";
+            if(base==11)
+                Sbase = "B";
+            if(base==12)
+                Sbase = "C";
+            if(base==13)
+                Sbase = "D";
+            if(base==14)
+                Sbase = "E";
+            if(base==15)
+                Sbase = "F";
+            if(base==16)
+                Sbase = "G";
+
             return ans;
         }
 
@@ -58,13 +145,11 @@ public class Ex1 {
          * Checks if the two numbers have the same value.
          * @param n1 first number
          * @param n2 second number
-         * @return true iff the two numbers have the same values.
+         * @return true if the two numbers have the same values.
          */
         public static boolean equals(String n1, String n2) {
             boolean ans = true;
-            // add your code here
 
-            ////////////////////
             return ans;
         }
 
--- Ex1Main.java
+++ Ex1Main.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
+import java.io.*;
 
 /**
  * Intro2CS, Ex1 - very basic "main template"
  * Make sure your implementation of this main performs as the Ex1Sol.jar solution implement all needed functions.
- *
  */
 public class Ex1Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int num3;
+        int basee;
         String num1 = "", num2="", quit = "quit";
         while (!num1.equals(quit) && !num2.equals(quit)) {
             System.out.println();
@@ -16,8 +18,13 @@ public class Ex1Main {
             num1 = sc.next();
             if (!num1.equals("quit")) {
                 // add your code here
-
-                /////////////////////
+                System.out.println(Ex1.isNumber(num1));
+                System.out.println(Ex1.number2Int(num1));
+                System.out.println("enter num3, base");
+                num3 = sc.nextInt();
+                basee = sc.nextInt();
+                System.out.println(Ex1.int2Number(num3,basee));
+                ///////////////////////
             }
         }
         System.out.println("quiting now...");
